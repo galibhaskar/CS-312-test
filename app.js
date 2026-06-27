d52e401adab1bcaed7e4d3be2939640a3a4e5434
VEERA SURYA BHASKAR GALI <30974013+galibhaskar@users.noreply.github.com>
2026-06-27 09:11:59 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ const app = express();
 const PORT = 5000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(session({
     secret: "blogsecret",
